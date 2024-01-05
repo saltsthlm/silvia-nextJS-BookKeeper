a49f76a24452b19e3f5dcd733fddceb3396970ee
Silvia Dominguez <silviadmgz@gmail.com>
2024-01-05 14:24:33 +0100

    fix: http request link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,11 +3,14 @@ import Link from 'next/link';
 export default function FirstPost() {
 	const handleRequest = () => {
 		const apiFetch = async () => {
-			const data = await fetch('http://localhost:5065/Books', {
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json' },
-				// body: JSON.stringify({ newWords: newWords }),
-			});
+			const data = await fetch(
+				'https://silvia-nextjs-bookkeeper-e3uzzxqbva-uc.a.run.app/Books',
+				{
+					method: 'GET',
+					headers: { 'Content-Type': 'application/json' },
+					// body: JSON.stringify({ newWords: newWords }),
+				}
+			);
 			const jsonData = await data.json();
 			console.log(jsonData);
 		};
